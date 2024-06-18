d433f7a58eb3d70e74bdb439bc66c94b05ffa0fd
Bastian Jakobi <bastian.jakobi2000@gmail.com>
2024-06-18 09:59:09 +0200

    fix: increase page load timeouts

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   video: true,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    pageLoadTimeout: 120000,
   },
 });
